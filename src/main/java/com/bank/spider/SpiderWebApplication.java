b523b7f3ade7c0f53b537fbcf71868770aaa0244
huwf <huwf@liandisys.com.cn>
2019-06-19 16:32:48 +0800

    20190619

---
+++ src/main/java/com/bank/spider/SpiderWebApplication.java
@@ -0,0 +1,13 @@
+package com.bank.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderWebApplication.class, args);
+	}
+
+}
